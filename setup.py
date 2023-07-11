61007b2419775544783bdb6fbea0be08e27dd0e1
Bijay Nayak <bijay6779@gmail.com>
2023-07-11 22:26:21 +0530

    Update Setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,37 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.0.0'
+DESCRIPTION = 'A Python library for interacting with PhonePe Payment Gateway API.'
+LONG_DESCRIPTION = 'This Python package provides a simple interface to the PhonePe Payment Gateway. It can be used to ' \
+                   'create PhonePe payment links, check the status of PhonePe transactions, and refund PhonePe ' \
+                   'transactions. '
+
 
 setup(
     name='phonepe',
-    version='1.0.0',
-    description='A Python library for interacting with PhonePe Payment Gateway API.',
+    version=VERSION,
+    description=DESCRIPTION,
     author='Bijay Nayak',
     author_email='bijay6779@gmail.com',
     url='https://github.com/devbijay/phonepe-pg',
-    packages=['phonepe'],
-    install_requires=[
-        'requests', 'pydantic',
-    ],
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['requests', 'pydantic'],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-    ],
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+    ,
 )
